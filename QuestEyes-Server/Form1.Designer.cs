d5fd9a390ca4f19d840c882628c27ae9fad1d49a
robotprobot <swheeler2013@outlook.com>
2022-01-29 00:32:19 +0000

    first commit

---
+++ QuestEyes-Server/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace QuestEyes_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.conStatLabel = new System.Windows.Forms.Label();
+            this.batPercentage = new System.Windows.Forms.Label();
+            this.firmwareVer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.conStat = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(144, 13);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(96, 20);
+            this.title.TabIndex = 0;
+            this.title.Text = "QuestEyes";
+            // 
+            // conStatLabel
+            // 
+            this.conStatLabel.AutoSize = true;
+            this.conStatLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conStatLabel.Location = new System.Drawing.Point(7, 51);
+            this.conStatLabel.Name = "conStatLabel";
+            this.conStatLabel.Size = new System.Drawing.Size(95, 13);
+            this.conStatLabel.TabIndex = 1;
+            this.conStatLabel.Text = "Connection status:";
+            // 
+            // batPercentage
+            // 
+            this.batPercentage.AutoSize = true;
+            this.batPercentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.batPercentage.Location = new System.Drawing.Point(7, 84);
+            this.batPercentage.Name = "batPercentage";
+            this.batPercentage.Size = new System.Drawing.Size(149, 13);
+            this.batPercentage.TabIndex = 2;
+            this.batPercentage.Text = "Battery percentage: Unknown";
+            // 
+            // firmwareVer
+            // 
+            this.firmwareVer.AutoSize = true;
+            this.firmwareVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firmwareVer.Location = new System.Drawing.Point(7, 97);
+            this.firmwareVer.Name = "firmwareVer";
+            this.firmwareVer.Size = new System.Drawing.Size(138, 13);
+            this.firmwareVer.TabIndex = 3;
+            this.firmwareVer.Text = "Firmware version: Unknown";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check for firmware updates";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(195, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Force reconnect QuestEyes device";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Left eye Y: Unknown";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Left eye X: Unknown";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(132, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Right eye Y: Unknown";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(132, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Right eye X: Unknown";
+            // 
+            // conStat
+            // 
+            this.conStat.AutoSize = true;
+            this.conStat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conStat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.conStat.Location = new System.Drawing.Point(99, 51);
+            this.conStat.Name = "conStat";
+            this.conStat.Size = new System.Drawing.Size(73, 13);
+            this.conStat.TabIndex = 10;
+            this.conStat.Text = "Disconnected";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Location = new System.Drawing.Point(-3, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(388, 40);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.conStat);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.firmwareVer);
+            this.Controls.Add(this.batPercentage);
+            this.Controls.Add(this.conStatLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "QuestEyes PC App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label conStatLabel;
+        private System.Windows.Forms.Label batPercentage;
+        private System.Windows.Forms.Label firmwareVer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label conStat;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ QuestEyes-Server/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace QuestEyes_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Create websocket client instance
+            //CHANGE THE IP TO BE PART OF THE SEARCH SYSTEM
+            string url = "ws://192.168.1.237:81";
+            WebSocket ws = new WebSocket(url);
+
+            ws.OnMessage += Ws_OnMessage;
+            ws.OnClose += Ws_OnClose;
+
+            Console.WriteLine("Attempting to connect to " + url);
+            ws.Connect();
+        }
+
+        private void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Received: " + e.Data);
+            if (e.IsBinary == false) //Message from system
+            {
+                //IF FIRMWARE_VER, TREAT AS FIRMWARE VERSION
+                //IF EXCESSIVE_FRAME_FAILURE, TREAT AS DEVICE ERROR, DISCONNECT AND EXPECT REBOOT
+                if (e.Data.Contains("FIRMWARE_VER"))
+                {
+                    string[] split = e.Data.Split(' ');
+                    Console.WriteLine("Successful connection confirmed.");
+                    conStat.Invoke((MethodInvoker)delegate
+                    {
+                        conStat.Text = "Connected";
+                        conStat.ForeColor = Color.Green;
+                    });
+                    firmwareVer.Invoke((MethodInvoker)delegate
+                    {
+                        firmwareVer.Text = "Firmware version: " + split[1];
+                    });
+                }
+            }
+            else //Image from system
+            {
+
+            }
+        }
+
+        private void Ws_OnClose(object sender, CloseEventArgs e)
+        {
+            Console.WriteLine("Connection was lost or couldn't be established");
+            conStat.Invoke((MethodInvoker)delegate
+            {
+                conStat.ForeColor = Color.FromArgb(192, 0, 0);
+                conStat.Text = "Disconnected";
+            });
+        }
+    }
+}
